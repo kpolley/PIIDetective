ef07b13c651654108bef8ebe43b9e11701f661dc
Kyle Polley <kyle@kpolley.com>
2024-11-19 14:32:13 -0600

    small fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,8 +10,6 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
